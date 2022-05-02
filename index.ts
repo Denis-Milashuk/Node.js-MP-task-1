84600a32f32e1d77974d915ac8cf6bae086a7992
Denis Milashuk <ilovehardrock@mail.ru>
2022-05-02 16:16:17 +0300

    task 1.1 fixed

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
-import 'events'
-const { Transform } = require('stream');
-
-const transformStream = new Transform();
-transformStream._transform = (chunk: { toString: () => string; }, encoding: any, callback: () => void) => {
-    let reversedString = chunk.toString().split('').reverse().join('').replace('\n', '') + '\n';
-    transformStream.push(reversedString);
-    callback();
-};
-
-process.stdin.pipe(transformStream).pipe(process.stdout)
+console.log('EPAM Node.js global mentoring program')
 
 
---
+++ src/module_1/task_1.ts
@@ -0,0 +1,11 @@
+import 'events'
+const { Transform } = require('stream');
+
+const transformStream = new Transform();
+transformStream._transform = (chunk: { toString: () => string; }, encoding: any, callback: () => void) => {
+    let reversedString = chunk.toString().split('').reverse().join('').replace('\n', '') + '\n';
+    transformStream.push(reversedString);
+    callback();
+};
+
+process.stdin.pipe(transformStream).pipe(process.stdout)
